d75819dfa0b026aa6d2d03f5ed3676d8be17e457
Garrett Pfingston <gpfingston@gmail.com>
2025-12-15 16:28:08 -0600

    added logic and passed test

--- main.py
+++ main.py
@@ -8,29 +8,47 @@ import sqlite3
 conn1 = sqlite3.connect('planets.db')
 
 # Select all
-pd.read_sql("""SELECT * FROM planets; """, conn1)
+planet_db = pd.read_sql("""SELECT * FROM planets; """, conn1)
+print(planet_db)
 
 # STEP 1
 # Replace None with your code
-df_no_moons = None
+df_no_moons = pd.read_sql("""
+    SELECT * FROM planets
+    WHERE num_of_moons = 0; """, conn1)
+print(df_no_moons)
 
 # STEP 2
 # Replace None with your code
-df_name_seven = None
+df_name_seven = pd.read_sql("""
+    SELECT name, mass 
+    FROM planets
+    WHERE length(name) = 7; """, conn1)
+print(df_name_seven)
 
 ##### Part 2: Advanced Filtering #####
 
 # STEP 3
 # Replace None with your code
-df_mass = None
+df_mass = pd.read_sql("""
+    SELECT name, mass FROM planets
+    WHERE mass <= 1.00; """, conn1)
+print(df_mass)
 
 # STEP 4
 # Replace None with your code
-df_mass_moon = None
+df_mass_moon = pd.read_sql("""
+    SELECT * FROM planets
+    WHERE mass < 1.00 AND num_of_moons >= 1; """, conn1)
+print(df_mass_moon)
 
 # STEP 5
 # Replace None with your code
-df_blue = None
+df_blue = pd.read_sql("""
+    SELECT name, color 
+    FROM planets
+    WHERE color LIKE '%blue%'; """, conn1)
+print(df_blue)
 
 ##### Part 3: Ordering and Limiting #####
 
@@ -41,19 +59,35 @@ df_blue = None
 conn2 = sqlite3.connect('dogs.db')
 
 # Select all
-pd.read_sql("SELECT * FROM dogs;", conn2)
+dog_db =pd.read_sql("SELECT * FROM dogs;", conn2)
+print(dog_db)
 
 # STEP 6
 # Replace None with your code
-df_hungry = None
+df_hungry = pd.read_sql("""
+    SELECT name, age, breed 
+    FROM dogs
+    WHERE hungry = 1
+    ORDER BY age ASC; """, conn2)
+print(df_hungry)
 
 # STEP 7
 # Replace None with your code
-df_hungry_ages = None
+df_hungry_ages = pd.read_sql("""
+    SELECT name, age, hungry 
+    FROM dogs
+    WHERE hungry = 1 AND age > 2 AND age < 8
+    ORDER BY name ASC; """, conn2)
+print(df_hungry_ages)
 
 # STEP 8
 # Replace None with your code
-df_4_oldest = None
+df_4_oldest = pd.read_sql("""
+    SELECT name, age, breed
+    FROM dogs
+    ORDER BY age DESC
+    LIMIT 4; """, conn2)
+print(df_4_oldest)
 
 
 ##### Part 4: Aggregation #####
@@ -65,27 +99,43 @@ df_4_oldest = None
 conn3 = sqlite3.connect('babe_ruth.db')
 
 # Select all
-pd.read_sql("""
+babe_ruth_db = pd.read_sql("""
 SELECT * FROM babe_ruth_stats; """, conn3)
+print(babe_ruth_db)
 
 # STEP 9
 # Replace None with your code
-df_ruth_years = None
+df_ruth_years = pd.read_sql("""
+    SELECT max(year) - min(year) + 1 AS total_years_played 
+    FROM babe_ruth_stats; """, conn3)
+print(df_ruth_years)
 
 # STEP 10
 # Replace None with your code
-df_hr_total = None
+df_hr_total = pd.read_sql("""
+    SELECT sum(HR) AS total_home_runs 
+    FROM babe_ruth_stats; """, conn3)
+print(df_hr_total)
 
 
 ##### Part 5: Grouping and Aggregation #####
 
 # STEP 11
 # Replace None with your code
-df_teams_years = None
+df_teams_years = pd.read_sql("""
+    SELECT team, COUNT(year) AS number_years
+    FROM babe_ruth_stats
+    GROUP BY team; """, conn3)
+print(df_teams_years)
 
 # STEP 12
 # Replace None with your code
-df_at_bats = None
+df_at_bats = pd.read_sql("""
+    SELECT team, round(AVG(at_bats)) AS average_at_bats
+    FROM babe_ruth_stats
+    GROUP BY team
+    HAVING AVG(at_bats) > 200; """, conn3)
+print(df_at_bats)
 
 
 conn1.close()
